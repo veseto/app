e071654581470f8141d0e970047f1bd263aa6d50
veseto <ludataaa@mail.bg>
2012-12-04 23:18:30 +0200

    Initial Commit

---
+++ Common/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Common
+//
+//  Created by Vesela Popova on 04.12.12.
+//  Copyright (c) 2012 г. Vesela Popova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
